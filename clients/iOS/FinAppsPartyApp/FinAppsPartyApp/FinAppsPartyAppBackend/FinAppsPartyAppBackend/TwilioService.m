d9f3b1d2478bb9320160c22f9cc335fa14ee9ebb
Tomasz Janeczko <janecztom@gmail.com>
2012-10-27 00:44:54 +0200

    [iOS] Complicated the Twilio Service more than needed to call it only once.

--- clients/iOS/FinAppsPartyApp/FinAppsPartyApp/FinAppsPartyAppBackend/FinAppsPartyAppBackend/TwilioService.m
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/FinAppsPartyAppBackend/FinAppsPartyAppBackend/TwilioService.m
@@ -12,25 +12,40 @@
 @implementation TwilioService
 
 - (void)tokenForTwilioWithSuccessBlock:(TwilioServiceSuccessfulBlock)successBlock failureBlock:(ServiceFailureBlock)failureBlock {
-    NSString *twilioServiceUrl = @"http://polar-springs-5033.herokuapp.com/client";
-    NSURL *url = [NSURL URLWithString:twilioServiceUrl];
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
-    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
-    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:urlRequest success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-        NSString *token = [JSON valueForKey:@"token"];
-        NSLog(@"Twilio token: %@", token);
-        successBlock(token);
-        
-    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-        UserError *userError = [UserError new];
-        userError.title = @"Twilio error.";
-        userError.message = [error description];
-        
-        failureBlock(userError);
-    }];
-    
-    [operation start];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+        static NSString *token = nil;
+
+        @synchronized(token) {
+            if (token) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    successBlock(token);
+                });
+                return;
+            }
+            
+            NSString *twilioServiceUrl = @"http://polar-springs-5033.herokuapp.com/client";
+            NSURL *url = [NSURL URLWithString:twilioServiceUrl];
+            NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+            [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            
+            AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:urlRequest success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                token = [JSON valueForKey:@"token"];
+                NSLog(@"Twilio token: %@", token);
+                successBlock(token);
+                
+            } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+                UserError *userError = [UserError new];
+                userError.title = @"Twilio error.";
+                userError.message = [error description];
+                
+                failureBlock(userError);
+            }];
+            
+            [operation start];
+        }
+    });
 }
 
 @end
