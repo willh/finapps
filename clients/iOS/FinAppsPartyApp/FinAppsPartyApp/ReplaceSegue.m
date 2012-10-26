2513e4a4b3110b2f49caf6217d6181f905dbe9f9
Kyle Davidson <k.davidson@kainos.com>
2012-10-26 20:34:57 +0100

    [iOS] Added custom replace segue

---
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/ReplaceSegue.m
@@ -0,0 +1,23 @@
+//
+//  ReplaceSegue.m
+//  FinAppsPartyApp
+//
+//  Created by Kyle Davidson on 26/10/2012.
+//  Copyright (c) 2012 Tomasz Janeczko. All rights reserved.
+//
+
+#import "ReplaceSegue.h"
+
+@implementation ReplaceSegue
+
+-(void) perform {
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    UIViewController *srcViewController = (UIViewController *) self.sourceViewController;
+    UIViewController *destViewController = (UIViewController *) self.destinationViewController;
+    [srcViewController.view removeFromSuperview];
+  
+    appDelegate.window.rootViewController=destViewController;
+}
+
+
+@end
