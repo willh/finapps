a61407c5a01ee2f64ed0bcde83a6a9bb4a887fe8
Kyle Davidson <k.davidson@kainos.com>
2012-10-26 15:13:22 +0100

    LoginVC and UseCaseOneVC created and hooked up.

---
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/LoginViewController.m
@@ -0,0 +1,70 @@
+//
+//  LoginViewController.m
+//  FinAppsPartyApp
+//
+//  Created by Kyle Davidson on 26/10/2012.
+//  Copyright (c) 2012 Tomasz Janeczko. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "UseCaseOneViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+@synthesize submitLoginButton;
+@synthesize usernameTextField, passwordTextField;
+@synthesize passwordMessageLabel, usernameMessageLabel, loginMessageLabel;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboards)];
+    [self.view addGestureRecognizer:gestureRecognizer];
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dismissKeyboards {
+    [usernameTextField resignFirstResponder];
+    [passwordTextField resignFirstResponder];
+}
+
+#pragma mark - Event Handlers
+
+- (IBAction)loginButtonTapped:(id)sender {
+    if ([usernameTextField.text isEqualToString:@""] || [passwordTextField.text isEqualToString:@""]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Unable to submit login credentials" message:@"Please provide a username and password" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+    } else {
+        //some verification
+        [self performSegueWithIdentifier:@"UseCaseOne" sender:self];
+    }
+
+}
+
+#pragma mark - UITextFieldDelegate Methods
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+@end
---
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/UseCaseOneViewController.h
@@ -0,0 +1,13 @@
+//
+//  UseCaseOneViewController.h
+//  FinAppsPartyApp
+//
+//  Created by Kyle Davidson on 26/10/2012.
+//  Copyright (c) 2012 Tomasz Janeczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UseCaseOneViewController : UIViewController
+
+@end
