4201b49bc15b536a76f7e36d479bb36f6b604e79
Kyle Davidson <k.davidson@kainos.com>
2012-10-27 00:02:00 +0100

    [iOS] Tab bar additions

---
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/CreditCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreditCardViewController.h
+//  FinAppsPartyApp
+//
+//  Created by Kyle Davidson on 27/10/2012.
+//  Copyright (c) 2012 Tomasz Janeczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditCardViewController : UIViewController
+
+@end
---
+++ clients/iOS/FinAppsPartyApp/FinAppsPartyApp/CreditCardViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreditCardViewController.m
+//  FinAppsPartyApp
+//
+//  Created by Kyle Davidson on 27/10/2012.
+//  Copyright (c) 2012 Tomasz Janeczko. All rights reserved.
+//
+
+#import "CreditCardViewController.h"
+
+@interface CreditCardViewController ()
+
+@end
+
+@implementation CreditCardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
